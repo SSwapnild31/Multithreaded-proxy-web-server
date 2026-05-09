b97e33f9265c3261c3bac88a79dac7598c8e9595
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-05-09 23:15:23 +0530

    Add files via upload

---
+++ http server/dns_resolver.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <netdb.h>
+
+#include "dns_resolver.h"
+
+int resolve_hostname(const char *host, char *ip){
+    
+    struct addrinfo hints;
+    struct addrinfo *res;
+
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if(getaddrinfo(host, NULL, &hints, &res) != 0){
+        return -1;
+    }
+
+    struct sockaddr_in *addr = (struct sockaddr_in*)res->ai_addr;
+
+    inet_ntop(AF_INET, &addr->sin_addr, ip, 100);
+
+    freeaddrinfo(res);
+
+    return 0;
+}
---
+++ http server/dns_resolver.h
@@ -0,0 +1,6 @@
+#ifndef DNS_RESOLVER_H
+#define DNS_RESOLVER_H
+
+int resolve_hostname(const char *host, char *ip);
+
+#endif
